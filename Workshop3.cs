87bf60bc1e9db4426fdd8bcb5feb3246622aaacd
odone88 <mate.rutkowski@gmail.com>
2017-08-12 18:16:35 +0200

    Add files via upload
    solution to workshop 3

---
+++ Workshop3.cs
@@ -0,0 +1,73 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace EndToEndMailCourse._03
+{
+    [TestFixture]
+    public class Workshop03Tests
+    {
+        private string testUrl = "https://suvroc.github.io/selenium-mail-course/03/workshop.html";
+
+        [Test]
+        public void ShouldCheckSelectedBookName()
+        {
+            var driver = new ChromeDriver();
+            driver.Navigate().GoToUrl(testUrl);
+
+            IWebElement book = null;
+
+            #region TEST CODE
+            book = driver.FindElementByCssSelector("body > div > div:nth-child(2) > div > div > ul > li.list-group-item.book.selected > h4:nth-child(1)");
+            #endregion
+
+            Assert.NotNull(book);
+            Assert.AreEqual(book.TagName, "h4");
+            Assert.AreEqual(book.Text, "MORT");
+
+            driver.Quit();
+        }
+
+        [Test]
+        public void ShouldCheckNumberOfBooks()
+        {
+            var driver = new ChromeDriver();
+            driver.Navigate().GoToUrl(testUrl);
+
+            IReadOnlyCollection<IWebElement> books = null;
+
+            #region TEST CODE
+            
+            books = driver.FindElementsByClassName("book");
+            #endregion
+
+            Assert.NotNull(books);
+            Assert.AreEqual(books.Count, 6);
+            Assert.IsTrue(books.All(x => x.TagName == "li"));
+            driver.Quit();
+        }
+
+        [Test]
+        public void ShouldCheckLinksDomain()
+        {
+            var driver = new ChromeDriver();
+            driver.Navigate().GoToUrl(testUrl);
+
+            IReadOnlyCollection<IWebElement> links = null;
+
+            #region TEST CODE
+            links = driver.FindElementsByLinkText("Details");
+            #endregion
+
+            Assert.NotNull(links);
+            Assert.AreEqual(links.Count, 6);
+            Assert.IsTrue(links.All(x => x.TagName == "a"));
+            Assert.IsTrue(links.All(x => x.GetAttribute("href").StartsWith("https://www.terrypratchettbooks.com")));
+
+            driver.Quit();
+        }
+    }
+}
