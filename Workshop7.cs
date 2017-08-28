33b9e6c8341dd109642a45eee955cb0c2d2f3e47
odone88 <mate.rutkowski@gmail.com>
2017-08-28 21:58:02 +0200

    Add files via upload
    Solution to Workshop 7

---
+++ Workshop7.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Linq;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Interactions;
+using System;
+
+namespace EndToEndMailCourse._07
+{
+    [TestFixture]
+    public class Workshop07Tests
+    {
+        private string testUrl = "https://suvroc.github.io/selenium-mail-course/07/workshop.html";
+
+        [Test]
+        public void ShouldSelectAnswers()
+        {
+            var driver = new ChromeDriver();
+            driver.Navigate().GoToUrl(testUrl);
+
+            var companySelectElement = driver.FindElement(By.Id("companySelect"));
+            var carSelectElement = driver.FindElement(By.Id("carSelect"));
+
+            #region TEST CODE
+            var selectCompany = new SelectElement(companySelectElement);
+            selectCompany.SelectByText("Ford");
+
+            var waiter = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            var waitCarSelect = waiter
+                .Until(ExpectedConditions.ElementToBeClickable(carSelectElement));
+
+            var selectCar = new SelectElement(carSelectElement);
+            selectCar.SelectByText("Sedan");
+
+            var waitForListOfDates = waiter.Until(ExpectedConditions.InvisibilityOfElementLocated(By.Id("tridots")));
+
+            #endregion
+
+            Assert.AreEqual(driver.FindElements(By.ClassName("list-group-item")).Count(), 6);
+
+
+            driver.Quit();
+        }
+    }
+}
